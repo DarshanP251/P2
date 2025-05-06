6b111527347b42a835e8c7ff08b6c23223d0e70c
DarshanP251 <darshanpatil0511@gmail.com>
2025-05-06 11:49:58 +0530

    Add StudentCourse class in Java

---
+++ StudentCourse.java
@@ -0,0 +1,25 @@
+package P2;
+
+
+import java.util.HashMap;
+
+public class StudentCourse {
+    HashMap<String, Integer> courses = new HashMap<>();
+
+    public void addCourse(String name, int marks) {
+        courses.put(name, marks);
+    }
+
+    public void displayCourses() {
+        for (String course : courses.keySet()) {
+            System.out.println(course + ": " + courses.get(course));
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentCourse sc = new StudentCourse();
+        sc.addCourse("Math", 95);
+        sc.addCourse("Science", 90);
+        sc.displayCourses();
+    }
+}
